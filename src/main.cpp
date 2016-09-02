37c67bf4bd9186ddd3d6e32d3d1af5f68ef127e6
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2016-09-02 15:32:05 +0300

    Add Led class and enable MedianFilter

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,32 @@
 #include "DisplayTM1637.h"
 
 #include "ButtonInterrupt.h"
+#include "MedianFilter.h"
 #include "DistanceSensorInterrupt.h"
+#include "Led.h"
+
+#define SHARP_PIN A0
+#define BUTTON_PIN 5
+#define LED_PIN 13
 
 RoboraceStopwatch *roboraceStopwatch;
 
 /**
  * Если в момент запуска нажата кнопка то прерывания будут фиксироваться по кнопке.
- * При рестарте необходимо держать кнопку, отпустить когда загорится светодиод на 13 ноге.
+ * При рестарте необходимо держать кнопку, отпустить когда загорится встроенный светодиод.
  * Без удержания кнопки будет использоваться датчик расстояния
  */
 Interrupt *createInterrupt() {
-    ButtonPullUp button(5);
+    ButtonPullUp button(BUTTON_PIN);
     if (button.read()) {
-        pinMode(13, OUTPUT);
-        digitalWrite(13, HIGH);
-        delay(1000);
-        digitalWrite(13, LOW);
-        delay(300);
-        return new ButtonInterrupt(5);
+        Led led(LED_PIN);
+        led.high();
+        delay(500);
+        led.low();
+        delay(500);
+        return new ButtonInterrupt(BUTTON_PIN);
     }
-    return new DistanceSensorInterrupt(new Sharp(A0));
+    return new DistanceSensorInterrupt(new MedianFilter(new Sharp(SHARP_PIN)));
 }
 
 void setup() {
